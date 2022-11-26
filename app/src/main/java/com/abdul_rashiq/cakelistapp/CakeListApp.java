0580d1208c730e02511b9f311a46cae034abcc44
Rashique <abdul@journeyhero.net>
2022-11-26 02:03:51 +0000

    Added libraries for pattern.

---
+++ app/src/main/java/com/abdul_rashiq/cakelistapp/CakeListApp.java
@@ -0,0 +1,14 @@
+package com.abdul_rashiq.cakelistapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class CakeListApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
